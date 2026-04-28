52362f9ada33f3ab99eb3b6f4298e97be24ce523
MarlyRamirez <mramireze9@miumg.edu.gt>
2026-04-27 22:01:22 -0600

    Se agregó la interfaz para lista enlazada para agregar y eliminar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'screens/list_screen.dart'; // Importa pantalla de lista
 
 void main() {
   runApp(const MyApp());
@@ -52,37 +54,45 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Proyecto Prog III'),
+        title: const Text('Estructuras de Datos'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const Icon(Icons.rocket_launch, size: 80, color: Colors.black),
+            const Icon(Icons.account_tree_outlined, size: 80, color: Colors.black),
             const SizedBox(height: 20),
             const Text(
-              '¡El proyecto base está listo!\nTema e integraciones iniciales.',
+              'Seleccione una estructura:',
               textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 18, 
-                fontWeight: FontWeight.bold,
-                color: Colors.black
-              ),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
+            const SizedBox(height: 30),
+            
+            // BOTÓN PARA TU MÓDULO (Marly)
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(vertical: 15),
+              ),
+              onPressed: () {
+                // Navegación hacia tu pantalla
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ListScreen()),
+                );
+              },
+              icon: const Icon(Icons.list_alt),
+              label: const Text('Lista Enlazada'),
+            ),
+            
+            const SizedBox(height: 10),
+            // Aquí agregarás los botones de Tabla Hash, Pilas, etc. después.
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Acción de prueba
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: const Text('¡Botón funcionando! ✅'),
-              backgroundColor: Colors.blueGrey.shade800,
-            )
-          );
-        },
-        child: const Icon(Icons.add),
       ),
     );
   }
